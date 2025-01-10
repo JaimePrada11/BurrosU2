31056f79cdae22188efe1f44051f6e5df9154dc2
JaimePrada11 <jandrespradato.11@gmail.com>
2025-01-10 01:15:05 -0500

    :tada: Creacion del proyecto

---
+++ src/main/java/campus/u2/burrosu2/BurrosU2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package campus.u2.burrosu2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class BurrosU2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
